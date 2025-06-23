13a2265543820e1fcafe06bb21492f05f394dea2
Rylie Castell <rylie.castell@gmail.com>
2025-06-23 22:57:05 +0200

    Initial Commit

---
+++ SpaceTradersTests/SpaceTradersTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceTradersTests.swift
+//  SpaceTradersTests
+//
+//  Created by Rylie Castell on 23.06.25.
+//
+
+import Testing
+@testable import SpaceTraders
+
+struct SpaceTradersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
