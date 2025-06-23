ef7e5c3e3b129c51d828d352a639d20aa3d66a74
Rylie Castell <rylie.castell@gmail.com>
2025-06-23 23:53:54 +0200

    change app target info

---
+++ SpaceTraders/Views/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  SpaceTraders
+//
+//  Created by Rylie Castell on 23.06.25.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
