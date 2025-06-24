eada8271f281499a32e4674c6ff5fec993554888
Rylie Castell <rylie.castell@gmail.com>
2025-06-25 00:14:59 +0200

    add get factions method

--- SpaceTraders/Services/SpaceTradersApi.swift
+++ SpaceTraders/Services/SpaceTradersApi.swift
@@ -35,4 +35,23 @@ class SpaceTradersApi {
         let result = try JSONDecoder().decode(AgentResponse.self, from: data)
         return result.data
     }
+    
+    func getFactions() async throws -> FactionsData {
+        guard let url = URL(string: baseUrl + "/my/factions") else {
+            throw HTTPError.invalidURL
+        }
+        
+        let headers = [
+            "Content-Type": "application/json",
+            "Authorization": "Bearer \(key)"
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let result = try JSONDecoder().decode(FactionsResponse.self, from: data)
+        return result.data
+    }
 }
